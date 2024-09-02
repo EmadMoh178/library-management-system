671c7dd14ecbe4391bfde769e7b3b30ead5b3982
EmadMoh178 <EmadMoh178@gmail.com>
2024-09-03 00:02:52 +0300

    Fix minor bug in a unit test

--- src/test/java/com/example/library_management/LibraryManagementApplicationTests.java
+++ src/test/java/com/example/library_management/LibraryManagementApplicationTests.java
@@ -13,6 +13,7 @@ class LibraryManagementApplicationTests {
 		Dotenv dotenv = Dotenv.configure().load();
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 	}
 
 	@Test
